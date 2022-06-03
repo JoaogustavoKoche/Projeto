6446e0246966276dd3161d77fa953e0278718811
igBFranco <igor.franco@aluno.unc.br>
2022-06-03 11:55:22 -0300

    adding remove certificate

--- js/certificates.js
+++ js/certificates.js
@@ -1,38 +1,75 @@
+function loadCertificates() {
+    var i = 0;
+    for (i; i < 5; i++) {
+        if(!localStorage.getItem(i)) {
+            return
+        }else {
+            loadItem(i);
+        }
+    }
+}
 
-var certificatesList = localStorage.getItem("certificatesList");
-certificatesList = JSON.parse(certificatesList);
-if(certificatesList == null){
-    certificatesList = [];
-} 
+function loadItem(i) {
+    var ul = document.getElementById("certificatesList");
+    var li = document.createElement("li");
+
+    
+    var input = document.createElement("input");
+    input.value = localStorage.getItem(i);
+    
+    input.setAttribute('id', ul.children.length);
+    input.setAttribute('value', input.value);
+    input.setAttribute('onkeyup', "saveValue(this)");
+    input.setAttribute('disabled', true);
+    localStorage.setItem(input.id, input.value);
+    
+    var edit = document.createElement("a");
+    edit.setAttribute('id', 'edit');
+    edit.setAttribute('name', ul.children.length);
+    edit.setAttribute('onclick', 'edit(this)');
+    
+    var remove = document.createElement("a");
+    remove.setAttribute('id', 'remove');
+    remove.setAttribute('name', ul.children.length);
+    remove.setAttribute('onclick', 'removeItem(this)')
+
+    li.setAttribute('id',input.value);
+    li.setAttribute('name',ul.children.length);
+    li.appendChild(input);
+    li.appendChild(edit);
+    li.appendChild(remove);
+    ul.appendChild(li);
 
 
+}
 
 function addItem(){
     var ul = document.getElementById("certificatesList");
     var certificate = document.getElementById("certificates");
     var li = document.createElement("li");
 
-    var cert = JSON.stringify({
-        1: ""
-    })
-
-    var remove = document.createElement("a");
-    remove.setAttribute('id', 'remove');
-    remove.setAttribute('onclick', 'removeItem(document.getElementById(certificates.value))')
-
+    
     var input = document.createElement("input");
     input.value = certificate.value;
+    
     input.setAttribute('id', ul.children.length);
+    input.setAttribute('value', certificate.value);
     input.setAttribute('onkeyup', "saveValue(this)");
     input.setAttribute('disabled', true);
     localStorage.setItem(input.id, input.value);
-
+    
     var edit = document.createElement("a");
     edit.setAttribute('id', 'edit');
     edit.setAttribute('name', ul.children.length);
     edit.setAttribute('onclick', 'edit(this)');
+    
+    var remove = document.createElement("a");
+    remove.setAttribute('id', 'remove');
+    remove.setAttribute('name', ul.children.length);
+    remove.setAttribute('onclick', 'removeItem(this)')
 
     li.setAttribute('id',certificate.value);
+    li.setAttribute('name',ul.children.length);
     li.appendChild(input);
     li.appendChild(edit);
     li.appendChild(remove);
@@ -43,12 +80,11 @@ function addItem(){
 }
 
 function removeItem(e){
-    let list = document.querySelector('#certificatesList')
-    var id = e.id;
+    let list = document.querySelector('#certificatesList');
     console.log("removendo");
-    console.log(id);
-    console.log(e);
-    list.removeChild(e);
+    console.log(e.name);
+    list.removeChild(e.parentNode);
+    localStorage.removeItem(e.name)
     
 }
 
--- js/storage3.js
+++ js/storage3.js
@@ -3,6 +3,13 @@ document.querySelector('#team').value = getSavedValue("team");
 document.querySelector('#institution').value = getSavedValue("institution");
 document.querySelector('#graduation').value = getSavedValue("graduation");
 
+//document.querySelector('#0').value = getSavedValue("0");
+//document.querySelector('#1').value = getSavedValue("1");
+//document.querySelector('#2').value = getSavedValue("2");
+//document.querySelector('#3').value = getSavedValue("3");
+//document.querySelector('#4').value = getSavedValue("4");
+
+
 function saveCertificates(e) {
     var certificate = {
         1: "",
